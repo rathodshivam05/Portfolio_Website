c83ad168ba369ca7b65456c35dae12e26c6f8e18
Shivam Rathod <rathodshivam05@gmail.com>
2022-07-01 19:49:17 +0530

    My Portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/rathodshivam05",
+    "name": "Shivam Rathod",
+    "role": "Full Stack Web Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to explore in various parts of full stack development and like to learn about new technologies, or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/shivam-rathod-763bb01a0/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/rathodshivam05",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am actively looking for a new opportunities. I am a self taught Full Stack Web Developer, currently diving deeper into MERN Stack. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/rathodshivam05",
+    "education":[
+      {
+        "UniversityName":"Savitribai Phule Pune University",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Mar",
+        "YearOfPassing":"2021",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Masai School",
+        "specialization":"MERN Stack Development",
+        "MonthOfPassing":"sep",
+        "YearOfPassing":"2022",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    // "work":[
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   },
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   }
+    // ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Kheliya Clone",
+        "imgurl":"images/portfolio/Kheliya.png"
+      },
+      {
+        "name":"project2",
+        "description":"Nature Basekt's Clone",
+        "imgurl":"https://miro.medium.com/max/700/0*mSKYRoN933ITxA2n.jpeg"
+      },
+      {
+        "name":"project3",
+        "description":"Libazz Fashion Studio",  
+        "imgurl":"images/portfolio/libazz.png"
+      },
+      {
+        "name":"project4",
+        "description":"Sugar Cosmetics Clone",
+        "imgurl":"images/portfolio/sugar.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
